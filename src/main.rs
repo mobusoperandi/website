47745ef930461130cb710fb51e76ce66e0a6e0ae
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-07-10 16:05:54 +0700

    rm reduntant into_iter

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn base(
           meta charset="utf-8";
           meta name="viewport" content="width=device-width, initial-scale=1.0";
           link rel="stylesheet" href={ "/index.css?v=" (version) };
-          @for stylesheet in stylesheets.into_iter() {
+          @for stylesheet in stylesheets {
               link rel="stylesheet" href=(stylesheet);
           }
         }
