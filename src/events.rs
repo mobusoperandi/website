8484b705e6e0a22536aa8bbeb0d8fb7e5443327f
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-09-03 07:58:41 +0700

    refactor: no use RRuleSet::all_before_with_error

--- src/events.rs
+++ src/events.rs
@@ -26,26 +26,22 @@ pub fn events() -> Vec<Event> {
                 .collect::<Vec<_>>()
         })
         .flat_map(|(mob, recurring_session)| {
-            let (occurrences, error) = recurring_session
-                .recurrence()
-                .clone()
-                .all_before_with_error(
-                    Utc::now().with_timezone(&Etc__UTC) + Duration::weeks(10),
-                    true,
-                    u16::MAX,
-                );
-            if let Some(error) = error {
-                panic!("{:?}", error);
-            }
+            let occurrences = recurring_session.recurrence();
             let duration = recurring_session.duration();
-            occurrences.into_iter().map(move |occurrence| Event {
-                start: occurrence.with_timezone(&Utc),
-                end: (occurrence + duration).with_timezone(&Utc),
-                title: mob.id().to_string(),
-                url: "/".to_string() + mob.id() + ".html",
-                background_color: mob.background_color().clone(),
-                text_color: mob.text_color().clone(),
-            })
+            occurrences
+                .into_iter()
+                .map(move |occurrence| Event {
+                    start: occurrence.with_timezone(&Utc),
+                    end: (occurrence + duration).with_timezone(&Utc),
+                    title: mob.id().to_string(),
+                    url: "/".to_string() + mob.id() + ".html",
+                    background_color: mob.background_color().clone(),
+                    text_color: mob.text_color().clone(),
+                })
+                .take_while(|event| {
+                    event.start <= Utc::now().with_timezone(&Etc__UTC) + Duration::weeks(10)
+                })
+                .collect::<Vec<_>>()
         })
         .collect()
 }
