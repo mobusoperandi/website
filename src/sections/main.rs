f6cf76a4b704821d701f24a407f93eb548f6fb2d
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-10-06 13:28:08 +0700

    redo main section

--- src/sections/main.rs
+++ src/sections/main.rs
@@ -1,21 +1,31 @@
-use crate::fonts;
-
 use super::Section;
 use maud::html;
 pub fn section() -> Section {
     Section {
         id: "main".into(),
-        classes: "".into(),
+        classes: [
+            "text-center",
+            "uppercase",
+            "tracking-widest",
+            "text-[6vw]",
+            "flex",
+            "flex-col",
+            "justify-around",
+        ]
+        .join(" "),
         stylesheet: None,
         content: html! {
-            h1 class=(format!("font-[{}]", fonts::VOLLKORN)) { "An online community for the regular study of software development in mob programming format" }
-            ul {
-                li { a href="#in_the_media" { "in_the_media" } }
-                li { a href="#mobs_calendar" { "mobs_calendar" } }
-                li { a href="#join" { "join" } }
-                li { a href="#mightyiam" { "mightyiam" } }
-                li { a href="#why_mob" { "why_mob" } }
+            h2 {
+                span { "Study" }
+                " "
+                span { "software development" }
+                " "
+                span { "online" }
+                " in "
+                span { "mob programming" }
+                " format."
             }
+            a href="#mobs_calendar" { "See existing mobs" }
         },
     }
 }
