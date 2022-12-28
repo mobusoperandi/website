4185fa478ed6c3a66d11ae8d909069fd147984b8
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-12-28 17:12:07 +0700

    move calendar link to logo

--- src/pages.rs
+++ src/pages.rs
@@ -46,12 +46,13 @@ pub(crate) fn base(
         }
         body class=(classes!("min-h-screen" "py-1" "px-1" "md:px-5" "flex" "flex-col" "gap-1" "max-w-screen-xl" "mx-auto")) {
             div class=(classes!("flex" "justify-between" "items-center" "flex-wrap" "gap-x-2" "gap-y-1" "uppercase" "text-lg")) {
-                div class=(classes!("flex" "flex-col" "gap-x-2" "whitespace-nowrap")) {
+                a
+                    href=(targets.relative("index.html").unwrap().to_str().unwrap())
+                    class=(classes!("flex" "flex-col" "gap-x-2" "whitespace-nowrap")) {
                     p class=(classes!("tracking-widest" "text-center")) { (NAME) }
                     p class=(classes!("text-sm" "opacity-75")) { (DESCRIPTION) }
                 }
                 div class=(classes!("flex" "flex-wrap" "gap-x-2")) {
-                    a href=(targets.relative("index.html").unwrap().to_str().unwrap()) { "Calendar" }
                     a href=(targets.relative("join.html").unwrap().to_str().unwrap()) { "Join" }
                 }
                 div class=(classes!("flex" "items-center" "gap-x-2")) {
