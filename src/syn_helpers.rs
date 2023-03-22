27cc959fffd2110bb172526d797debe0a44dc115
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-03-22 13:54:58 +0700

    update schema and syn
    Co-authored-by: Kelly Rankin <kellyprankin@gmail.com>

--- src/syn_helpers.rs
+++ src/syn_helpers.rs
@@ -1,29 +1,24 @@
-use schema::proc_macro2;
-
 pub(crate) trait Attribute {
     fn doc_string(&self) -> Option<String>;
 }
 
 impl Attribute for syn::Attribute {
     fn doc_string(&self) -> Option<String> {
-        let last_path_segment = self.path.segments.last()?;
+        let syn::Meta::NameValue(name_value) = &self.meta else {
+            return None;
+        };
+
+        let last_path_segment = name_value.path.segments.last()?;
 
         if last_path_segment.ident != "doc" {
             return None;
         }
 
-        let value_tokens = self
-            .tokens
-            .clone()
-            .into_iter()
-            .skip(1)
-            .collect::<proc_macro2::TokenStream>();
-
-        let Ok(literal) = syn::parse2::<syn::Lit>(value_tokens) else {
+        let syn::Expr::Lit(literal_expression) = &name_value.value else {
             return None;
         };
 
-        let syn::Lit::Str(lit_str) = literal else {
+        let syn::Lit::Str(lit_str) = &literal_expression.lit else {
             return None;
         };
 
