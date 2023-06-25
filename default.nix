6b95cb93361c35e00443dee609a27feeb56481d2
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-06-25 19:57:45 +0700

    ssg: use notify and not cargo-watch
    closes #57
    
    Co-authored-by: José Manuel Peña <josemanuelp2@gmail.com>

--- default.nix
+++ default.nix
@@ -4,21 +4,6 @@
   replaceStrings = builtins.replaceStrings;
   concatStringsSep = builtins.concatStringsSep;
 
-  cargo-run-bin = pkgs.rustPlatform.buildRustPackage rec {
-    pname = "cargo-run-bin";
-    version = "0.4.1";
-    src = pkgs.fetchCrate {
-      inherit pname version;
-      sha256 = "sha256-Muy21AuCpHCOO/XNds5Tn07d4mjhuyk1KzbCQdBT0bQ=";
-    };
-    cargoLock = {
-      lockFile = src + "/Cargo.lock";
-    };
-    nativeBuildInputs = with pkgs; [
-      cargo-llvm-cov
-    ];
-  };
-
   targetTriple = "x86_64-unknown-linux-gnu";
   envTargetTriple = pipe targetTriple [
     toUpper
@@ -29,7 +14,6 @@ in
     nativeBuildInputs = with pkgs; [
       tokio-console
       rustup
-      cargo-run-bin
       mob
       nodejs-18_x
       nodePackages.typescript-language-server
--- ssg-parent/src/rebuild_and_run.rs
+++ ssg-parent/src/rebuild_and_run.rs
@@ -1,36 +1,65 @@
+use std::path::PathBuf;
+
+use async_fn_stream::fn_stream;
+use futures::TryStreamExt;
+use notify::{recommended_watcher, Event, EventKind, RecursiveMode, Watcher};
 use thiserror::Error;
-use tokio::process::Command;
+use tokio::{
+    process::{Child, Command},
+    sync::mpsc,
+};
 
 #[derive(Error, Debug)]
 pub enum WatchError {
     #[error(transparent)]
     Io(#[from] std::io::Error),
-    #[error("{0}")]
-    Exit(std::process::ExitStatus),
+    #[error(transparent)]
+    Notify(#[from] notify::Error),
 }
 
 pub async fn watch_for_changes_and_rebuild() -> WatchError {
-    let child = Command::new("cargo")
-        .args([
-            "bin",
-            "cargo-watch",
-            "--workdir",
-            "builder",
-            "--exec",
-            "run",
-        ])
-        .spawn();
-
-    let mut child = match child {
+    let child = match cargo_run() {
         Ok(child) => child,
-        Err(err) => return err.into(),
+        Err(error) => return error.into(),
     };
 
-    // success case is indefinitely waiting here
-    let status = match child.wait().await {
-        Ok(status) => status,
-        Err(err) => return err.into(),
+    let (sender, mut receiver) = mpsc::channel(1);
+
+    let watcher = recommended_watcher(move |result: Result<Event, notify::Error>| {
+        sender.blocking_send(result).unwrap();
+    });
+
+    let mut watcher = match watcher {
+        Ok(watcher) => watcher,
+        Err(error) => return error.into(),
     };
 
-    WatchError::Exit(status)
+    if let Err(error) = watcher.watch(&PathBuf::from("builder"), RecursiveMode::Recursive) {
+        return error.into();
+    }
+
+    fn_stream(|emitter| async move {
+        while let Some(event) = receiver.recv().await {
+            emitter.emit(event).await;
+        }
+    })
+    .try_fold(child, |mut child, event: Event| async move {
+        if let EventKind::Create(_) | EventKind::Modify(_) | EventKind::Remove(_) = event.kind {
+            child.kill().await?;
+            Ok(cargo_run()?)
+        } else {
+            Ok(child)
+        }
+    })
+    .await
+    .expect_err("should end only in the case of error")
+    .into()
+}
+
+fn cargo_run() -> Result<Child, std::io::Error> {
+    let child = Command::new("cargo")
+        .args(["run", "--package=builder"])
+        .spawn()?;
+
+    Ok(child)
 }
