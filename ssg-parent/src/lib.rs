d7b60053414671af77354305f716f02e586fe3b4
Remy Clarke <60509493+ClarkeRemy@users.noreply.github.com>
2023-10-12 22:36:14 +0900

    ssg parent struct
    Co-authored-by: Roland Fredenhagen <dev@modprog.de>

--- ssg-parent/src/lib.rs
+++ ssg-parent/src/lib.rs
@@ -1,4 +1,16 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 mod dev;
-pub use dev::{dev, DevError};
+pub use dev::DevError;
+
+pub struct Parent {
+    output_dir: camino::Utf8PathBuf,
+}
+
+impl Parent {
+    pub fn new(output_dir: impl Into<camino::Utf8PathBuf>) -> Self {
+        Self {
+            output_dir: output_dir.into(),
+        }
+    }
+}
